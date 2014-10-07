3497ed6079d6451a9c276cce724f99f20e622ba1
arloard <arloard3@gmail.com>
2014-10-07 23:30:11 +0400

    nsmth
    this is also nothing

---
+++ android/src/com/ltfl/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ltfl.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ltfl.LFTL;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LFTL(), config);
+	}
+}
